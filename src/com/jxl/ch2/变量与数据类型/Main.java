3fb911429a6124733669ff459bf8cc64d8b8e6a9
zerohope <a584171811@gmail.com>
2021-03-27 16:46:22 +0800

    class source of ch2

---
+++ src/com/jxl/ch2/变量与数据类型/Main.java
@@ -0,0 +1,12 @@
+package com.jxl;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println(System.getProperty("java.version"));
+
+
+
+    }
+
+
+}
---
+++ src/com/jxl/ch2/变量与数据类型/UseNewFeature.java
@@ -0,0 +1,13 @@
+package com.jxl;
+
+public class UseNewFeature {
+
+    public static void main(String[] args) {
+        var info=  """
+                  好好学习。
+                  天天向上
+                   """;
+        System.out.println(info);
+    }
+
+}
